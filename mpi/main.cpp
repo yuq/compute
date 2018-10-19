19f9e884ee919c25b3df03ceb9acdc68ba981bd6
Qiang Yu <Qiang.Yu@amd.com>
2018-10-19 15:02:34 +0800

    add mpi

---
+++ mpi/main.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <hip/hip_runtime.h>
+
+#define CHECK(cmd) \
+{ \
+    hipError_t error  = cmd; \
+    if (error != hipSuccess) { \
+        fprintf(stderr, "error: '%s'(%d) at %s:%d\n", \
+		hipGetErrorString(error), error,__FILE__, __LINE__); \
+        exit(EXIT_FAILURE); \
+    } \
+}
+
+char h_buffer[] = "hello";
+
+void sender(void)
+{
+	char *d_buffer;
+	CHECK(hipMalloc(&d_buffer, sizeof(h_buffer)));
+	CHECK(hipMemcpy(d_buffer, h_buffer, sizeof(h_buffer), hipMemcpyHostToDevice));
+	
+	MPI_Send(d_buffer, sizeof(h_buffer), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+}
+
+void receiver(void)
+{
+	char *d_buffer;
+	CHECK(hipMalloc(&d_buffer, sizeof(h_buffer)));
+	
+	MPI_Status status;
+	MPI_Recv(d_buffer, sizeof(h_buffer), MPI_CHAR, 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+	char result[100];
+	CHECK(hipMemcpy(result, d_buffer, sizeof(h_buffer), hipMemcpyDeviceToHost));
+	printf("result: %s\n", result);
+}
+
+int main(int argc, char **argv)
+{
+	hipDeviceProp_t props;
+	CHECK(hipGetDeviceProperties(&props, 0));
+	printf("info: running on device %s\n", props.name);
+
+	int pid=-1, np=-1;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	if (pid)
+		sender();
+	else
+		receiver();
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	MPI_Finalize();
+	return 0;
+}
