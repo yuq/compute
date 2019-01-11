d31a3deaa7b80db89d6b4d8b515e56f5988b65db
Qiang Yu <Qiang.Yu@amd.com>
2019-01-11 10:11:52 +0800

    ipc: update

--- ipc/main.c
+++ ipc/main.c
@@ -20,8 +20,10 @@ static hsa_status_t test_hsa_agent_callback(hsa_agent_t agent, void* data)
 {
 	hsa_device_type_t device_type;
 	assert(hsa_agent_get_info(agent, HSA_AGENT_INFO_DEVICE, &device_type) == HSA_STATUS_SUCCESS);
-	if (device_type != HSA_DEVICE_TYPE_GPU)
+	if (device_type != HSA_DEVICE_TYPE_GPU) {
+		printf("found cpu agent %lu\n", agent.handle);
 		return HSA_STATUS_SUCCESS;
+	}
 
 	struct agents *agents = data;
 	assert(agents->num < MAX_GPU);
@@ -33,11 +35,12 @@ static hsa_status_t test_hsa_agent_callback(hsa_agent_t agent, void* data)
 
 static hsa_agent_t get_agent(int idx)
 {
-	assert(hsa_init() == HSA_STATUS_SUCCESS);
+	static struct agents agents = {0};
 
-	struct agents agents = {0};
-	hsa_status_t status = hsa_iterate_agents(test_hsa_agent_callback, &agents);
-	assert(status == HSA_STATUS_SUCCESS || status == HSA_STATUS_INFO_BREAK);
+	if (agents.num == 0) {
+		hsa_status_t status = hsa_iterate_agents(test_hsa_agent_callback, &agents);
+		assert(status == HSA_STATUS_SUCCESS || status == HSA_STATUS_INFO_BREAK);
+	}
 
 	assert(idx < agents.num);
 	return agents.agents[idx];
@@ -85,21 +88,35 @@ static hsa_amd_memory_pool_t get_pool(hsa_agent_t agent)
 
 static void parent(int fd)
 {
-	hsa_agent_t agent = get_agent(0);
-	hsa_amd_memory_pool_t pool = get_pool(agent);
+	assert(hsa_init() == HSA_STATUS_SUCCESS);
+
+	hsa_agent_t src_agent = get_agent(0);
+	hsa_amd_memory_pool_t pool = get_pool(src_agent);
 
 	hsa_status_t status;
 	void *src_ptr;
 	status = hsa_amd_memory_pool_allocate(pool, BUFF_SIZE, 0, &src_ptr);
 	assert(status == HSA_STATUS_SUCCESS);
+	memset(src_ptr, 0x23, BUFF_SIZE);
 
 	hsa_amd_ipc_memory_t ipc = {0};
 	assert(read(fd, &ipc, sizeof(ipc)) == sizeof(ipc));
 
 	void *dst_ptr;
-	status = hsa_amd_ipc_memory_attach(&ipc, BUFF_SIZE, 1, &agent, &dst_ptr);
+	hsa_agent_t dst_agent = get_agent(1);
+	hsa_agent_t import_agents[2] = {src_agent, dst_agent};
+	status = hsa_amd_ipc_memory_attach(&ipc, BUFF_SIZE, 2, import_agents, &dst_ptr);
 	assert(status == HSA_STATUS_SUCCESS);
 
+	hsa_amd_pointer_info_t info = {
+		.size = sizeof(hsa_amd_pointer_info_t),
+	};
+	status = hsa_amd_pointer_info(dst_ptr, &info, NULL, NULL, NULL);
+	assert(status == HSA_STATUS_SUCCESS);
+	printf("dst type=%d agentbase=%p hostbase=%p own=%lu\n",
+	       info.type, info.agentBaseAddress, info.hostBaseAddress,
+	       info.agentOwner.handle);
+	
 	hsa_signal_t signal;
 	status = hsa_signal_create(1, 0, NULL, &signal);
 	assert(status == HSA_STATUS_SUCCESS);
@@ -107,8 +124,8 @@ static void parent(int fd)
 	struct timespec tv1, tv2;
 	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
 	
-	status = hsa_amd_memory_async_copy(dst_ptr, agent,
-					   src_ptr, agent,
+	status = hsa_amd_memory_async_copy(dst_ptr, dst_agent,
+					   src_ptr, src_agent,
 					   BUFF_SIZE, 0,
 					   NULL, signal);
 	assert(status == HSA_STATUS_SUCCESS);
@@ -117,6 +134,9 @@ static void parent(int fd)
 				       (uint64_t)-1, HSA_WAIT_STATE_ACTIVE));
 
 	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
+
+	uint32_t *data = dst_ptr;
+	printf("parent result %x %x %x %x\n", data[0], data[1], data[2], data[3]);
 
 	double start = tv1.tv_sec;
 	start = start * 1e9 + tv1.tv_nsec;
@@ -132,6 +152,8 @@ static void parent(int fd)
 
 static void child(int fd)
 {
+	assert(hsa_init() == HSA_STATUS_SUCCESS);
+
 	hsa_agent_t agent = get_agent(1);
 	hsa_amd_memory_pool_t pool = get_pool(agent);
 
@@ -140,6 +162,15 @@ static void child(int fd)
 	status = hsa_amd_memory_pool_allocate(pool, BUFF_SIZE, 0, &ptr);
 	assert(status == HSA_STATUS_SUCCESS);
 
+	hsa_amd_pointer_info_t info = {
+		.size = sizeof(hsa_amd_pointer_info_t),
+	};
+	status = hsa_amd_pointer_info(ptr, &info, NULL, NULL, NULL);
+	assert(status == HSA_STATUS_SUCCESS);
+	printf("ptr type=%d agentbase=%p hostbase=%p own=%lu\n",
+	       info.type, info.agentBaseAddress, info.hostBaseAddress,
+	       info.agentOwner.handle);
+
 	hsa_amd_ipc_memory_t ipc = {0};
 	status = hsa_amd_ipc_memory_create(ptr, BUFF_SIZE, &ipc);
 	assert(status == HSA_STATUS_SUCCESS);
@@ -147,6 +178,10 @@ static void child(int fd)
 	assert(write(fd, &ipc, sizeof(ipc)) == sizeof(ipc));
 
 	sleep(5);
+
+	uint32_t *data = ptr;
+	printf("child result %x %x %x %x\n", data[0], data[1], data[2], data[3]);
+
 	hsa_shut_down();
 }
 
