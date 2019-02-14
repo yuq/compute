07440d49ede0ccaf7f0e95f2a9170d485b952e83
Qiang Yu <Qiang.Yu@amd.com>
2019-02-14 11:03:48 +0800

    hip-copy: add D2D P2P H2H

--- hip-copy/main.c
+++ hip-copy/main.c
@@ -10,7 +10,7 @@
 
 #define BUFF_SIZE (1 << 25)
 
-void test(int p2d, int use_hip)
+void test_dp(int p2d, int use_hip)
 {
     void *src, *dst;
     int kind;
@@ -55,11 +55,120 @@ void test(int p2d, int use_hip)
     }
 }
 
+void test_dd(int cross_gpu)
+{
+    void *src, *dst;
+
+    hipSetDevice(cross_gpu);
+    assert(hipMalloc(&dst, BUFF_SIZE) == hipSuccess);
+    hipSetDevice(0);
+    assert(hipMalloc(&src, BUFF_SIZE) == hipSuccess);
+
+    hipStream_t stream;
+    assert(hipStreamCreate(&stream) == hipSuccess);
+
+    struct timespec tv1, tv2;
+    assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
+
+    assert(hipMemcpyDtoDAsync(dst, src, BUFF_SIZE, stream) == hipSuccess);
+    assert(hipDeviceSynchronize() == hipSuccess);
+
+    assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
+
+    double start = tv1.tv_sec;
+    start = start * 1e9 + tv1.tv_nsec;
+
+    double end = tv2.tv_sec;
+    end = end * 1e9 + tv2.tv_nsec;
+
+    double rate = BUFF_SIZE / (end - start);
+    printf("%s D2D copy rate %f GB/s\n", cross_gpu ? "inter" : "intra", rate);
+
+    hipFree(dst);
+    hipFree(src);
+}
+
+void test_pp_one(void *dst, void *src)
+{
+    struct timespec tv1, tv2;
+	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
+
+    //hipMemcpy(dst, src, BUFF_SIZE, hipMemcpyHostToHost);
+    memcpy(dst, src, BUFF_SIZE);
+
+    assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
+
+    double start = tv1.tv_sec;
+	start = start * 1e9 + tv1.tv_nsec;
+
+	double end = tv2.tv_sec;
+	end = end * 1e9 + tv2.tv_nsec;
+
+	double rate = BUFF_SIZE / (end - start);
+	printf("P2P copy rate %f GB/s\n", rate);
+}
+
+void test_pp(void)
+{
+    void *src, *dst;
+
+    assert(hipMallocHost(&dst, BUFF_SIZE) == hipSuccess);
+    assert(hipMallocHost(&src, BUFF_SIZE) == hipSuccess);
+
+    for (int i = 0; i < 10; i++)
+        test_pp_one(dst, src);
+
+    hipFreeHost(dst);
+    hipFreeHost(src);
+}
+
+void test_hh_one(void *dst, void *src)
+{
+    struct timespec tv1, tv2;
+	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
+
+    memcpy(dst, src, BUFF_SIZE);
+
+    assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
+
+    double start = tv1.tv_sec;
+	start = start * 1e9 + tv1.tv_nsec;
+
+	double end = tv2.tv_sec;
+	end = end * 1e9 + tv2.tv_nsec;
+
+	double rate = BUFF_SIZE / (end - start);
+	printf("H2H copy rate %f GB/s\n", rate);
+}
+
+void test_hh(void)
+{
+    void *dst = malloc(BUFF_SIZE);
+    assert(dst);
+
+    void *src = malloc(BUFF_SIZE);
+    assert(src);
+
+    for (int i = 0; i < 10; i++)
+        test_hh_one(dst, src);
+
+    free(dst);
+    free(src);
+}
+
 int main(void)
 {
-    test(0, 0);
-    test(1, 0);
-    test(0, 1);
-    test(1, 1);
+    test_dp(0, 0);
+    test_dp(1, 0);
+    test_dp(0, 1);
+    test_dp(1, 1);
+
+    test_pp();
+
+    test_hh();
+
+    test_dd(0);
+    test_dd(1);
+
     return 0;
 }
