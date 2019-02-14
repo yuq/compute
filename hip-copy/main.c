02ece7e8435e861d7548e368a7932580aea079ce
Qiang Yu <Qiang.Yu@amd.com>
2019-02-14 14:13:14 +0800

    hip-copy: add p2h/h2p

--- hip-copy/main.c
+++ hip-copy/main.c
@@ -88,7 +88,7 @@ void test_dd(int cross_gpu)
     hipFree(src);
 }
 
-void test_pp_one(void *dst, void *src)
+void test_copy_one(const char *name, void *dst, void *src)
 {
     struct timespec tv1, tv2;
 	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
@@ -105,7 +105,7 @@ void test_pp_one(void *dst, void *src)
 	end = end * 1e9 + tv2.tv_nsec;
 
 	double rate = BUFF_SIZE / (end - start);
-	printf("P2P copy rate %f GB/s\n", rate);
+	printf("%s copy rate %f GB/s\n", name, rate);
 }
 
 void test_pp(void)
@@ -116,29 +116,10 @@ void test_pp(void)
     assert(hipMallocHost(&src, BUFF_SIZE) == hipSuccess);
 
     for (int i = 0; i < 10; i++)
-        test_pp_one(dst, src);
+        test_copy_one("P2P", dst, src);
 
     hipFreeHost(dst);
     hipFreeHost(src);
-}
-
-void test_hh_one(void *dst, void *src)
-{
-    struct timespec tv1, tv2;
-	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
-
-    memcpy(dst, src, BUFF_SIZE);
-
-    assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
-
-    double start = tv1.tv_sec;
-	start = start * 1e9 + tv1.tv_nsec;
-
-	double end = tv2.tv_sec;
-	end = end * 1e9 + tv2.tv_nsec;
-
-	double rate = BUFF_SIZE / (end - start);
-	printf("H2H copy rate %f GB/s\n", rate);
 }
 
 void test_hh(void)
@@ -150,10 +131,40 @@ void test_hh(void)
     assert(src);
 
     for (int i = 0; i < 10; i++)
-        test_hh_one(dst, src);
+        test_copy_one("H2H", dst, src);
 
     free(dst);
     free(src);
+}
+
+void test_ph(int p2h)
+{
+    void *dst, *src;
+
+    if (p2h) {
+        dst = malloc(BUFF_SIZE);
+        assert(dst);
+
+        assert(hipMallocHost(&src, BUFF_SIZE) == hipSuccess);
+    }
+    else {
+        src = malloc(BUFF_SIZE);
+        assert(src);
+
+        assert(hipMallocHost(&dst, BUFF_SIZE) == hipSuccess);
+    }
+
+    for (int i = 0; i < 10; i++)
+        test_copy_one(p2h ? "P2H" : "H2P", dst, src);
+
+    if (p2h) {
+        free(dst);
+        hipFreeHost(src);
+    }
+    else {
+        free(src);
+        hipFreeHost(dst);
+    }
 }
 
 int main(void)
@@ -166,6 +177,9 @@ int main(void)
     test_pp();
 
     test_hh();
+
+    test_ph(0);
+    test_ph(1);
 
     test_dd(0);
     test_dd(1);
