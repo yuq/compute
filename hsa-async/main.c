c9a31aba5c5638b38593b5bda55b9d2470660c58
Qiang Yu <yuq825@gmail.com>
2021-01-07 17:39:10 +0800

    hsa-async: alignment

--- hsa-async/main.c
+++ hsa-async/main.c
@@ -113,12 +113,12 @@ static hsa_region_t get_gtt_region(hsa_agent_t agent)
 
 static void print_info(void *ptr, const char *name)
 {
-    hsa_status_t status;
+	hsa_status_t status;
 	hsa_amd_pointer_info_t info = {
 		.size = sizeof(hsa_amd_pointer_info_t),
 	};
 
-    status = hsa_amd_pointer_info(ptr, &info, NULL, NULL, NULL);
+	status = hsa_amd_pointer_info(ptr, &info, NULL, NULL, NULL);
 	assert(status == HSA_STATUS_SUCCESS);
 	printf("%s type=%d agentbase=%p hostbase=%p own=%lu\n", name,
 	       info.type, info.agentBaseAddress, info.hostBaseAddress,
@@ -127,74 +127,74 @@ static void print_info(void *ptr, const char *name)
 
 static hsa_agent_t get_mem_agent(void *ptr)
 {
-    hsa_status_t status;
+	hsa_status_t status;
 	hsa_amd_pointer_info_t info = {
 		.size = sizeof(hsa_amd_pointer_info_t),
 	};
 
-    status = hsa_amd_pointer_info(ptr, &info, NULL, NULL, NULL);
+	status = hsa_amd_pointer_info(ptr, &info, NULL, NULL, NULL);
 	assert(status == HSA_STATUS_SUCCESS);
-    return info.agentOwner;
+	return info.agentOwner;
 }
 
 int main(int argc, char **argv)
 {
-    hsa_status_t status;
+	hsa_status_t status;
 
 	assert(hsa_init() == HSA_STATUS_SUCCESS);
 
-    hsa_agent_t agent0 = get_agent(0);
+	hsa_agent_t agent0 = get_agent(0);
 	hsa_region_t vram_region = get_vram_region(agent0);
 
-    void *vram_ptr[BUFF_NUM];
-    for (int i = 0; i < BUFF_NUM; i++) {
-        status = hsa_memory_allocate(vram_region, BUFF_SIZE, vram_ptr + i);
-        assert(status == HSA_STATUS_SUCCESS);
-        print_info(vram_ptr[i], "vram");
-    }
+	void *vram_ptr[BUFF_NUM];
+	for (int i = 0; i < BUFF_NUM; i++) {
+		status = hsa_memory_allocate(vram_region, BUFF_SIZE, vram_ptr + i);
+		assert(status == HSA_STATUS_SUCCESS);
+		print_info(vram_ptr[i], "vram");
+	}
 
 	hsa_agent_t agent1 = get_agent(1);
 	hsa_region_t gtt_region = get_gtt_region(agent1);
 
-    void *gtt_ptr[BUFF_NUM];
-    for (int i = 0; i < BUFF_NUM; i++) {
-        status = hsa_memory_allocate(gtt_region, BUFF_SIZE, gtt_ptr + i);
-        assert(status == HSA_STATUS_SUCCESS);
-        print_info(gtt_ptr[i], "gtt");
-    }
+	void *gtt_ptr[BUFF_NUM];
+	for (int i = 0; i < BUFF_NUM; i++) {
+		status = hsa_memory_allocate(gtt_region, BUFF_SIZE, gtt_ptr + i);
+		assert(status == HSA_STATUS_SUCCESS);
+		print_info(gtt_ptr[i], "gtt");
+	}
 
-    hsa_signal_t signal[BUFF_NUM];
-    for (int i = 0; i < BUFF_NUM; i++) {
-        status = hsa_signal_create(1, 0, NULL, signal + i);
-        assert(status == HSA_STATUS_SUCCESS);
-    }
+	hsa_signal_t signal[BUFF_NUM];
+	for (int i = 0; i < BUFF_NUM; i++) {
+		status = hsa_signal_create(1, 0, NULL, signal + i);
+		assert(status == HSA_STATUS_SUCCESS);
+	}
 
-    struct timespec tv1, tv2;
+	struct timespec tv1, tv2;
 	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv1));
 
-    for (int i = 0; i < BUFF_NUM; i++) {
-        status = hsa_amd_memory_async_copy(gtt_ptr[0], agent1,
-                                           vram_ptr[0], agent0,
-                                           BUFF_SIZE, 0, NULL,
-                                           signal[i]);
-        assert(status == HSA_STATUS_SUCCESS);
-    }
+	for (int i = 0; i < BUFF_NUM; i++) {
+		status = hsa_amd_memory_async_copy(gtt_ptr[0], agent1,
+						   vram_ptr[0], agent0,
+						   BUFF_SIZE, 0, NULL,
+						   signal[i]);
+		assert(status == HSA_STATUS_SUCCESS);
+	}
 
-    for (int i = 0; i < BUFF_NUM; i++) {
-        /*
-        while (hsa_signal_wait_acquire(signal[i], HSA_SIGNAL_CONDITION_LT, 1,
-                                       UINT64_MAX, HSA_WAIT_STATE_ACTIVE));
-        */
-        while (1) {
-            hsa_signal_value_t v = hsa_signal_load_scacquire(signal[i]);
-            printf("v=%ld\n", v);
-        }
-        //while (hsa_signal_load_scacquire(signal[i]) != 0);
-    }
+	for (int i = 0; i < BUFF_NUM; i++) {
+		/*
+		  while (hsa_signal_wait_acquire(signal[i], HSA_SIGNAL_CONDITION_LT, 1,
+		  UINT64_MAX, HSA_WAIT_STATE_ACTIVE));
+		*/
+		while (1) {
+			hsa_signal_value_t v = hsa_signal_load_scacquire(signal[i]);
+			printf("v=%ld\n", v);
+		}
+		//while (hsa_signal_load_scacquire(signal[i]) != 0);
+	}
 
-    assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
+	assert(!clock_gettime(CLOCK_MONOTONIC_RAW, &tv2));
 
-    double start = tv1.tv_sec;
+	double start = tv1.tv_sec;
 	start = start * 1e9 + tv1.tv_nsec;
 
 	double end = tv2.tv_sec;
@@ -204,5 +204,5 @@ int main(int argc, char **argv)
 	printf("copy rate %f GB/s\n", rate);
 
 	hsa_shut_down();
-    return 0;
+	return 0;
 }
